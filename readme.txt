9e990374b5eecd05909c714fb556b9a6346d966a
HuanCheng Bai <best.tony@foxmail.com>
2018-01-15 01:23:14 +0800

    0.0.1

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Simple Plyr ===
+Contributors: bestony
+Tags: video
+Requires at least: 4.6.0
+Tested up to: 4.9.1
+Requires PHP: 5.5
+Stable tag: trunk
+License: GPL-2.0+
+
+A Simple Plyr Video Player Plugin, You can use shortcode [plyr] to add a video.
+
+== Description ==
+A Simple Plyr Video Player Plugin, You can use shortcode [plyr] to add a video.
+
+Now, Support paramter url and poster
+
+== Installation ==
+1. Download The Plugin
+2. Upload the Plugin 
+3. Active the plugin
+
+== Changelog ==
+== 0.0.1 ==
+* init project
--- simple-plyr.php
+++ simple-plyr.php
@@ -21,3 +21,47 @@
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
+
+// Add Shortcode
+function plyr_filter( $atts ) {
+
+    // Attributes
+    $atts = shortcode_atts(
+        array(
+            'url' => '/path/to/video.mp4',
+            'poster' => '/path/to/poster.jpg',
+        ),
+        $atts,
+        'plyr'
+    );
+
+    $str = sprintf("<video poster='%s' controls><source src='%s' type='video/mp4'></video><script>plyr.setup();</script>",$atts['poster'],$atts['url']);
+
+    return $str;
+}
+add_shortcode( 'plyr', 'plyr_filter' );
+
+function plyr_assets() {
+    $plugin_url = plugin_dir_url( __FILE__ );
+    wp_register_style( 'plyr-style',$plugin_url . 'assets/plyr.css');
+    wp_enqueue_style( 'plyr-style' );
+
+    wp_register_script ( 'plyr-script',$plugin_url . 'assets/plyr.js' );
+    wp_enqueue_script ( 'plyr-script' );
+}
+add_action( 'wp_enqueue_scripts', 'plyr_assets' );
+
+
+// Add Quicktags
+function plyr_quicktags() {
+
+    if ( wp_script_is( 'quicktags' ) ) {
+    ?>
+    <script type="text/javascript">
+    QTags.addButton( 'plyr', 'Simple Plyr', '[plyr url="/path/to/video.mp4" poster="/path/to/poster.jpg"]', '', '', 'Plyr Video Player', 141 );
+    </script>
+    <?php
+    }
+
+}
+add_action( 'admin_print_footer_scripts', 'plyr_quicktags' );
