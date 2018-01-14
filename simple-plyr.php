839dd7f4b6a67e222daa350b31d6adcee7c0df91
HuanCheng Bai <best.tony@foxmail.com>
2018-01-15 00:40:31 +0800

    init

---
+++ simple-plyr.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @link              https://github.com/bestony/simple-plyr
+ * @since             0.0.1
+ * @package           Simple Plyr
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple Plyr
+ * Plugin URI:        https://github.com/bestony/simple-plyr
+ * Description:       Simple Plyr Video Player
+ * Version:           0.0.1
+ * Author:            Bestony
+ * Author URI:        https://github.com/bestony
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plyrio
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
